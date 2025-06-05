f5cc9b8cf61b023dccd4e4bf8f065c0837e41d45
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2025-06-06 00:21:46 +0200

    initial setup with .env nuxt and supa

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+  },
+});
