48966b8036b8f6c553e0c456ab44ebed51e78699
Ioannis Deligiannidis <ioannis.deligiannidis@student.uibk.ac.at>
2025-08-20 23:09:00 +0200

    t

--- pages/profile/[publicId].vue
+++ pages/profile/[publicId].vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router";
-import { ref, onMounted } from "vue";
 import UserProfile from "~/components/user/UserProfile.vue";
 
 definePageMeta({
